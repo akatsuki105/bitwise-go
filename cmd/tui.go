92132fd7f6baa9562f3ce1c71c83414d5c9a127c
akatsuki-py <bluejapan73+dev@gmail.com>
2020-05-08 08:49:59 +0900

    wip

--- cmd/tui.go
+++ cmd/tui.go
@@ -24,16 +24,18 @@ type TUIState struct {
 	bits     bitArray
 	dec      int
 	oct, hex string
+	count    int
 }
 
 func useTUI() {
 	state := TUIState{
 		mode:   mode32Bit,
-		cursor: [2]int{0, 31},
-		bits:   *newBitArray(31),
+		cursor: [2]int{2, 31},
+		bits:   *newBitArray(-1),
 		dec:    0,
 		oct:    "0",
 		hex:    "0",
+		count:  0,
 	}
 
 	if err := ui.Init(); err != nil {
@@ -69,15 +71,27 @@ func useTUI() {
 				state.mode = modeBinary
 				p0.BorderStyle.Fg = ui.ColorWhite
 				p1.BorderStyle.Fg = ui.ColorBlue
+				state.bits.update(int64(state.dec), state.cursor[1])
+				p0.Text = getP0Text(&state)
+				p1.Text = getP1Text(&state)
 			case modeBinary:
 				state.mode = mode32Bit
 				p0.BorderStyle.Fg = ui.ColorBlue
 				p1.BorderStyle.Fg = ui.ColorWhite
+				state.bits.update(int64(state.dec), -1)
+				p0.Text = getP0Text(&state)
+				p1.Text = getP1Text(&state)
 			}
 			ui.Render(p0, p1)
 
 		case "<Left>":
 			switch state.mode {
+			case mode32Bit:
+				state.cursor[0]++
+				if state.cursor[0] > 2 {
+					state.cursor[0] = 0
+				}
+				p0.Text = getP0Text(&state)
 			case modeBinary:
 				state.cursor[1]++
 				if state.cursor[1] > 31 {
@@ -90,6 +104,12 @@ func useTUI() {
 
 		case "<Right>":
 			switch state.mode {
+			case mode32Bit:
+				state.cursor[0]--
+				if state.cursor[0] < 0 {
+					state.cursor[0] = 2
+				}
+				p0.Text = getP0Text(&state)
 			case modeBinary:
 				state.cursor[1]--
 				if state.cursor[1] < 0 {
@@ -140,22 +160,63 @@ func useTUI() {
 			}
 			ui.Render(p0, p1)
 		}
+
+		if state.mode == mode32Bit {
+			state.count++
+			switch state.cursor[0] {
+			case 2: // dec
+				dec := int64(state.dec)
+				switch e.ID {
+				case "0":
+					if state.dec > 0 {
+						dec *= 10
+					}
+				case "1", "2", "3", "4", "5", "6", "7", "8", "9":
+					num, _ := strconv.Atoi(e.ID)
+					dec = dec*10 + int64(num)
+				case "<C-<Backspace>>":
+					dec /= 10
+				}
+				state.oct = strconv.FormatInt(dec, 8)
+				state.dec = int(dec)
+				state.hex = strconv.FormatInt(dec, 16)
+				state.bits.update(dec, -1)
+				p0.Text = getP0Text(&state)
+				p1.Text = getP1Text(&state)
+				ui.Render(p0, p1)
+			case 1: // hex
+			case 0: // oct
+			}
+		}
 	}
 }
 
 func getP0Text(state *TUIState) string {
-	header := "[Decimal:](fg:green)              [Hexdecimal:](fg:green)            [Octal:](fg:green)            "
+	header := "[Decimal:              Hexdecimal:           Octal:           ](fg:green)"
 	footer := fmt.Sprintf("%d", state.dec)
 	length := len(footer)
+	if state.mode == mode32Bit && state.cursor[0] == 2 {
+		footer = fmt.Sprintf("[%d](fg:blue)", state.dec)
+	}
 	for i := 0; i < 22-length; i++ {
 		footer += " "
 	}
-	footer += state.hex
+
+	if state.mode == mode32Bit && state.cursor[0] == 1 {
+		footer += fmt.Sprintf("[%s](fg:blue)", state.hex)
+	} else {
+		footer += state.hex
+	}
 	for i := 0; i < 23-len(state.hex); i++ {
 		footer += " "
 	}
-	footer += state.oct
-	text := header + "\n\n" + footer
+
+	if state.mode == mode32Bit && state.cursor[0] == 0 {
+		footer += fmt.Sprintf("[%s](fg:blue)", state.oct)
+	} else {
+		footer += state.oct
+	}
+	text := header + "\n \n" + footer
 	return text
 }
 
